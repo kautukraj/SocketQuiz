6017c079121568aa1ccaac678f37546e9b198030
Kautuk Raj <spamkraj@gmail.com>
2020-03-28 10:17:14 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -5,37 +5,52 @@ import time
 # please use functions :(
 serverPort = 1025
 serverSocket = socket(AF_INET, SOCK_STREAM)  # create a new socket using the given address family and socket type
-# SOCK_STREAM = TCP socket
-serverSocket.bind(('localhost', serverPort))
-serverSocket.listen(3)
+# SOCK_STREAM = TCP socket and AF_INET = IPv4
+serverSocket.bind(('localhost', serverPort))  # bind the server to given port and hostname
+serverSocket.listen(3)  # can connect to a maximum of three clients
 print("Server is setup")
-QnA = {"Q1": "A1", "Q2": "A2"}
-connections = []
-addresses = []
-names = []
-score = {}
+QnA = {"Q1": "A1", "Q2": "A2"}  # dictionary of questions(keys) and answers(values)
+connections = []  # list to store the client sockets
+addresses = []  # list to store client addresses
+names = []  # list to store client names (given as user input)
+score = {}  # dictionary to store scores of all participants
+no_of_players = 2  # will change this to 3 later
 
-for i in range(0, 1):
-    connectionSocket, address = serverSocket.accept()
-    name = (connectionSocket.recv(1024)).decode()
-    serverSocket.setblocking(True)
+for i in range(0, no_of_players):
+    connectionSocket, address = serverSocket.accept()  # accept client connections
+    name = (connectionSocket.recv(1024)).decode()  # receive client names
+    serverSocket.setblocking(True)  # IDK what this does exactly
     connections.append(connectionSocket)
     names.append(name)
-    score[name] = 0
+    score[name] = 0  # initialize score of each to 0
     print("Connected to ", address, name)
 
 while len(QnA) != 0:
-    question = random.choice(list(QnA.keys()))
-    answer = QnA[question]
-    QnA.pop(question)
+    question = random.choice(list(QnA.keys()))  # choosing a random question
+    answer = QnA[question]  # corresponding answer
+    QnA.pop(question)  # remove that question(and subsequently answer) from the dict to ensure no repetitions
     print("Your question is: ", question)
 
-    for i in range(0, 1):
-        connections[i].send(bytes(question, "utf-8"))
+    for client in connections:
+        client.send(bytes(question, "utf-8"))  # send question to all the players(clients)
+        client.send(bytes("Buzz by pressing any key", "utf-8"))
 
-    # find out who buzzed
-    buzzed = names[0]
+    who_buzzed = None  # store index of buzzing player
+    buzz = None  # truth value of buzzer, on or off. None = off; anything else = on
+    start_time = time.time()
 
+    while who_buzzed is None and time.time() - start_time < 10:  # while nobody has buzzed and 10 seconds have not
+        # elapsed
+        for client in connections:
+            try:
+                buzz = client.recv(1024).decode()
+            except:
+                buzz = None
+            if buzz is not None:
+                who_buzzed = client
+                break
+
+    # countdown timer
     for i in range(10, 0, -1):
         sys.stdout.write(str(i) + ' ')
         sys.stdout.flush()
